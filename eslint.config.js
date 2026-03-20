dd344aebd35d8353ed8539ce7fb822276bad7380
drill <nitrodrill@ya.ru>
2026-03-21 00:43:35 +0200

    allow hashbang in cli entry eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -86,5 +86,11 @@ export default tseslint.config(
             "@typescript-eslint/array-type": "off",
             "@typescript-eslint/no-unused-vars": "off",
         },
+    },
+    {
+        files: ["src/cli.ts"],
+        rules: {
+            "n/hashbang": "off",
+        },
     }
 );
