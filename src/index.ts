7b8ac0a15508cd739e954efd7b02793d74ac0e74
drill <nitrodrill@ya.ru>
2026-04-22 12:08:44 +0300

    refactor exports in index

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,33 @@ export * from "./interfaces/block-source.js";
 export * from "./interfaces/chain.js";
 export * from "./interfaces/db.js";
 export * from "./interfaces/leader-lock.js";
+export * from "./interfaces/logger.js";
 export * from "./interfaces/pipeline.js";
+export * from "./interfaces/reaction.js";
 export * from "./interfaces/repositories.js";
 export * from "./interfaces/runtime.js";
 export * from "./interfaces/transaction-manager.js";
 export * from "./interfaces/worker-lifecycle.js";
-export * from "./interfaces/reaction.js";
-export * from "./interfaces/logger.js";
 
-export * from "./workers/polling-worker.js";
-export * from "./workers/singleton-polling-worker.js";
 export * from "./workers/head-worker.js";
 export * from "./workers/fetch-worker.js";
 export * from "./workers/sequencer-worker.js";
 export * from "./workers/retention-worker.js";
-export * from "./workers/event-reaction-worker.js";
 export * from "./workers/transaction-reaction-worker.js";
+export * from "./workers/event-reaction-worker.js";
 
 export * from "./loggers/console-logger.js";
-export * from "./postgres/schema.js";
+
 export * from "./adapters/ethers-block-source.js";
+
+export * from "./postgres/schema.js";
 export * from "./postgres/leader-lock.js";
 export * from "./postgres/transaction-manager.js";
+
 export * from "./repositories/postgres/block-jobs-repository.js";
-export * from "./repositories/postgres/chain-cursor-repository.js";
 export * from "./repositories/postgres/canonical-blocks-repository.js";
 export * from "./repositories/postgres/canonical-events-repository.js";
-export * from "./repositories/postgres/raw-blocks-repository.js";
 export * from "./repositories/postgres/canonical-transactions-repository.js";
+export * from "./repositories/postgres/chain-cursor-repository.js";
+export * from "./repositories/postgres/raw-blocks-repository.js";
 export * from "./repositories/postgres/worker-cursors-repository.js";
