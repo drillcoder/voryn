28ca892c176cf3af6c5925c43d6962e6a3f7ec7d
drill <nitrodrill@ya.ru>
2026-05-26 23:17:17 +0300

    add github actions ci

--- dev/jest.config.js
+++ dev/jest.config.js
@@ -10,6 +10,14 @@ const config = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*.d.ts'],
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
